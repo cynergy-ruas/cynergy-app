ce599d54cafc8f910b4be6f639d29eef5c5820e2
Aryan <aryanjalali1234@gmail.com>
2019-02-03 12:32:42 +0530

    I have added the qr painter/generator. Ignore the commented code in main.dart. Painter doesnt have appbar. please pull before proceeding. @shyamant.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,22 @@
 import 'package:flutter/material.dart';
 import 'pages/root/root_page.dart';
 import 'auth/login_auth.dart';
-import 'pages/qrPage/qrCore.dart';
+import 'pages/qr/painter.dart';
+import 'pages/qr/qrCore.dart';
 
 void main() => runApp(MyApp());
+
+/*class Aplo extends StatelessWidget{
+@override 
+ Widget build(BuildContext context){
+ return new MaterialApp(
+   title: "Qr page ",
+   home: MainScreen(),
+   debugShowCheckedModeBanner:false
+     );
+ }
+}*/
+
 
 class MyApp extends StatelessWidget{
   @override
--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,1 +1,17 @@
 import 'package:flutter/material.dart';
+
+class Dash extends StatelessWidget{
+  final barColor= const Color(0xFF1C2331);
+  final bgColor1= const Color(0xFF001636);
+  final bgColor2= const Color(0xFF030E1D);
+Widget build(context){
+return MaterialApp(
+home: Scaffold(
+
+)
+
+);
+
+}
+
+}
